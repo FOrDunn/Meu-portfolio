afecefff4456979b1131ff5459d2eac6888121f7
Juan.Blandi <cruzjuanblandi@gmail.com>
2022-03-31 01:31:48 +0200

    Ajustando o efeito da Navbar

--- src/components/header/navBar.js
+++ src/components/header/navBar.js
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
+    const location = useLocation().pathname;
+    
     return (
         <nav className="navBar">
-            <Link to="/" className="navBarItems">Home</Link>
-            <Link to="/projetos" className="navBarItems">Projetos</Link>
-            <Link to="/contato" className="navBarItems">Contato</Link>
+            <Link to="/" className="navBarItems" data-selected={location === '/'}>Home</Link>
+            <Link to="/projetos" className="navBarItems" data-selected={location=== '/projetos'}>Projetos</Link>
+            <Link to="/contato" className="navBarItems" data-selected={location === '/contato'}>Contato</Link>
         </nav>
     );
 }
