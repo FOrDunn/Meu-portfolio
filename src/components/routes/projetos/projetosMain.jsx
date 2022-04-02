2931963e7b174271a4a5dea57f1d30a26ed850e1
Juan.Blandi <cruzjuanblandi@gmail.com>
2022-04-02 03:36:03 +0200

    Ajustando a pagina de projetos

--- src/components/routes/projetos/projetosMain.jsx
+++ src/components/routes/projetos/projetosMain.jsx
@@ -7,6 +7,30 @@ function Projetosmain() {
     return (
         <main className="projetosMain">
             <Projetostitle />
+            <div className="projetosWrapper">
+                <div className="projetosButton"><p></p></div>
+                <div className="projetosDiv">
+                    <div className="leftSectionProjetos">
+                        <div className="projetoImage"><img/></div>
+                        <h2 className="projetoTech">Tecnologias</h2>
+                        <div className="techIcons">
+                            <img/>
+                            <img/>
+                            <img/>
+                            <img/>
+                        </div>
+                    </div>
+                    <div className="rightSectionProjetos">
+                        <h2 className="projetoName">Portfolio</h2>
+                        <p className="projetoDescription">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type specimen book.
+                          </p>
+                    </div>
+                </div>
+                <div className="projetosButton"></div>
+            </div>
         </main>
     );
 }
